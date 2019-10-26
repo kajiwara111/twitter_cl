ad67d0822d3a5b290f2ec559215eecd706290b66
kajiwara333 <kajiwara@skydisc.jp>
2019-10-26 17:19:22 +0900

    initial commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,32 @@
+class TweetsController < ApplicationController
+    def index
+        @tweets = Tweet.all
+    end
+    
+    def new
+        @tweet = Tweet.new
+    end         
+
+    def create  
+        @tweet = Tweet.new(tweet_params)
+        if @tweet.save
+            redirect_to tweets_path, notice:"tweetを投稿しました"
+        else
+            flash.now[:alert] = 'tweetは１文字以上１４０文字以内で投稿してください'
+            render new_tweet_path
+        end
+    end
+
+    def show
+    end
+
+    def destroy
+
+    end
+
+    def tweet_params
+        params.require(:tweet).permit(:content)
+    end
+
+
+end
